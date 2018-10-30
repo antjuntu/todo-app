a5beaa858cb8470303262187985f2d49b7782339
antjuntu <antjuntu@gmail.com>
2018-10-30 17:21:48 +0200

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const url = 'mongodb://antti:salasana1@ds145053.mlab.com:45053/todo-app'
 
 mongoose.Promise = global.Promise
-mongoose.connect(url)
+mongoose.connect(url, { useNewUrlParser: true })
 
 module.exports = {
   mongoose
